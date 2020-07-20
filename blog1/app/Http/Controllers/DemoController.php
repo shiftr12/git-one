7e26ff7f3786297e1211151e6342ba49fcd8cbc6
shiftr12 <1295503859@qq.com>
2020-07-20 17:45:25 +0800

    res

--- blog1/app/Http/Controllers/DemoController.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Http\Controllers;
-//http://localhost/blog/public/index.php/DemoController/test
-class DemoController extends Controller{
-	public function test(){
-		return 1;
-	}
-}
